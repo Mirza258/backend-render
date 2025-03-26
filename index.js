a9f8c9287bcd746456033b0e1895bff852ea3f71
Mirza Omercic <mirza.os632@gmail.com>
2025-03-26 18:24:33 +0100

    port

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.put("/api/notes/:id", (req, res) => {
   res.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
